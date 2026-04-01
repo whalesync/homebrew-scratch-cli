86cbce9975b206ca1781ac295af446982e33ab19
GitLab CI <ci@whalesync.com>
2026-04-01 17:41:18 +0000

    Scratch Desktop v0.1.1-desktop

---
+++ Casks/scratch-desktop@0.rb
@@ -0,0 +1,23 @@
+cask "scratch-desktop@0" do
+  version "0.1.1"
+
+  if Hardware::CPU.arm?
+    url "https://github.com/whalesync/scratch-cli/releases/download/v0.1.1-desktop/Scratch%20Desktop-0.1.1-arm64.zip"
+    sha256 ""
+  else
+    url "https://github.com/whalesync/scratch-cli/releases/download/v0.1.1-desktop/Scratch%20Desktop-0.1.1-x64.zip"
+    sha256 ""
+  end
+
+  name "Scratch Desktop"
+  desc "Scratch content management desktop app"
+  homepage "https://github.com/whalesync/scratch-cli"
+
+  app "Scratch Desktop.app"
+
+  zap trash: [
+    "~/Library/Application Support/Scratch Desktop",
+    "~/Library/Preferences/com.scratch.desktop.plist",
+    "~/Library/Caches/com.scratch.desktop",
+  ]
+end
